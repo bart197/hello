c1d5e0bf5cd22cf00d181ed59f07158e2970bde2
Bart Allgood <bart197@users.noreply.github.com>
2018-06-19 09:52:03 -0600

    Updated to use args[0] when available

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String... args) {
-        System.out.println("Hello world!");
+        System.out.println(args.length > 0 ? args[0] : "Hello world!");
     }
 }
 
